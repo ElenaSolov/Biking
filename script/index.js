5c7ca0f8722c40a40afe35ee612a0019b1a3809b
Elena Soloveva <seaver@yandex.ru>
2022-01-05 13:51:29 +0200

    Fix: fix reviewer comments

--- script/index.js
+++ script/index.js
@@ -5,8 +5,8 @@ const switchBtns = page.querySelectorAll("input[type='checkbox']");
 switchBtns.forEach(btn => btn.addEventListener('click', switchTheme))
 
 function switchTheme(evt) {
-  if (evt.target.checked) switchBtns.forEach(btn => btn.checked = true);
-  page.classList.toggle('page_theme_dark');
+    if (evt.target.checked) switchBtns.forEach(btn => btn.checked = true);
+    page.classList.toggle('page_theme_dark');
 }
 
 // navigation
@@ -16,105 +16,114 @@ const mobileMenu = page.querySelector('.header__mobile-menu');
 const mobileNavLinks = mobileMenu.querySelectorAll('.header__link');
 
 burgerBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('header__mobile-menu_visible');
-  burgerBtn.classList.toggle('header__burger-menu_opened');
+    mobileMenu.classList.toggle('header__mobile-menu_visible');
+    burgerBtn.classList.toggle('header__burger-menu_opened');
 })
 
 
 // smooth scroll
 
 for (let link of mainNavLinks) {
-  link.addEventListener('click', smoothScroll);
+    link.addEventListener('click', smoothScroll);
 }
 
 for (let mobLink of mobileNavLinks) {
-  mobLink.addEventListener('click', smoothScroll)
+    mobLink.addEventListener('click', smoothScroll)
 }
 
 function smoothScroll(evt) {
-  evt.preventDefault()
+    evt.preventDefault()
 
-  const blockID = evt.target.getAttribute('href').slice(1);
+    const blockID = evt.target.getAttribute('href').slice(1);
 
-  document.getElementById(blockID).scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  })
+    document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
 
-  mobileMenu.classList.remove('header__mobile-menu_visible');
-  burgerBtn.classList.remove('header__burger-menu_opened');
+    mobileMenu.classList.remove('header__mobile-menu_visible');
+    burgerBtn.classList.remove('header__burger-menu_opened');
 }
 
-// high-road galery
+// high-road gallery
 
+const highRoadSliderWindow = page.querySelector('.high-road__gallery');
 const highRoadPrevBtn = page.querySelector('.high-road__btn_type_prev');
 const highRoadNextBtn = page.querySelector('.high-road__btn_type_next');
 const highRoadTitleEl = page.querySelector('.high-road__title');
 const highRoadTextEl = page.querySelector('.high-road__text');
 const highRoadImgEls = page.querySelectorAll('.high-road__img');
 const highRoadSignEl = page.querySelector('.high-road__list-item');
+
 let currentIndex = 0;
 
 const highRoadContent = [{
-  title: 'Шоссе',
-  text: 'На&nbsp;шоссейном велосипеде можно ездить по&nbsp;асфальту на&nbsp;разных градиентах: будь&nbsp;то горы или равнины. Гонки проходят в&nbsp;командном пелотоне, но&nbsp;тренироваться можно и&nbsp;самостоятельно.',
-  link: './images/high-road1.jpg',
-  alt: 'Фотография горного шоссе',
-  sign: 'high-road__list-item high-road__list-item_type_highroad'
+    title: 'Шоссе',
+    text: 'На&nbsp;шоссейном велосипеде можно ездить по&nbsp;асфальту на&nbsp;разных градиентах: будь&nbsp;то горы или равнины. Гонки проходят в&nbsp;командном пелотоне, но&nbsp;тренироваться можно и&nbsp;самостоятельно.',
+    link: './images/high-road1.jpg',
+    alt: 'Фотография горного шоссе',
+    sign: 'high-road__list-item high-road__list-item_type_highroad'
 },
-{
-  title: 'Грэвел',
-  text: 'Грэвел похож на&nbsp;шоссейный велосипед, но&nbsp;конструкция рамы немного отличается, и&nbsp;на&nbsp;нём стоят более широкие покрышки, всё для того чтобы проехать по&nbsp;лёгкому бездорожью.',
-  link: './images/high-road2.jpg',
-  alt: 'Фотография велосипедиста на лесной дорожке',
-  sign: 'high-road__list-item high-road__list-item_type_gravel'
-},
-{
-  title: 'TT',
-  text: 'ТТ&nbsp;&mdash; это велосипед для триатлона или раздельного старта, гооняют на&nbsp;таком велике только по&nbsp;равнинному асфальту, велик очень быстрые и&nbsp;аэродинамичный.',
-  link: './images/high-road3.jpg',
-  alt: 'Фотография равнинной дороги',
-  sign: 'high-road__list-item high-road__list-item_type_tt'
-}
+    {
+        title: 'Грэвел',
+        text: 'Грэвел похож на&nbsp;шоссейный велосипед, но&nbsp;конструкция рамы немного отличается, и&nbsp;на&nbsp;нём стоят более широкие покрышки, всё для того чтобы проехать по&nbsp;лёгкому бездорожью.',
+        link: './images/high-road2.jpg',
+        alt: 'Фотография велосипедиста на лесной дорожке',
+        sign: 'high-road__list-item high-road__list-item_type_gravel'
+    },
+    {
+        title: 'TT',
+        text: 'ТТ&nbsp;&mdash; это велосипед для триатлона или раздельного старта, гооняют на&nbsp;таком велике только по&nbsp;равнинному асфальту, велик очень быстрые и&nbsp;аэродинамичный.',
+        link: './images/high-road3.jpg',
+        alt: 'Фотография равнинной дороги',
+        sign: 'high-road__list-item high-road__list-item_type_tt'
+    }
 ]
+window.onload = function () {
+    getHighRoadSliderWindow();
+}
 
 highRoadNextBtn.addEventListener('click', showNextSection);
 highRoadPrevBtn.addEventListener('click', showPrevSection);
 
-function showNextSection() {
-  currentIndex++;
-  if (currentIndex == highRoadContent.length) currentIndex = 0;
-  highRoadTitleEl.textContent = highRoadContent[currentIndex].title;
-  highRoadTextEl.innerHTML = highRoadContent[currentIndex].text;
-  highRoadImgEls[0].src = highRoadContent[currentIndex].link;
-  highRoadImgEls[0].alt = highRoadContent[currentIndex].alt;
-  highRoadSignEl.className = highRoadContent[currentIndex].sign;
+function getHighRoadSliderWindow() {
+    highRoadSliderWindow.style.maxHeight = highRoadImgEls[0].height - 1 + 'px';
+}
 
-  if (currentIndex + 1 < highRoadContent.length) {
-    highRoadImgEls[1].src = highRoadContent[currentIndex + 1].link;
-    highRoadImgEls[1].alt = highRoadContent[currentIndex + 1].alt;
-  } else {
-    highRoadImgEls[1].src = highRoadContent[0].link;
-    highRoadImgEls[1].alt = highRoadContent[0].alt;
-  }
+function showNextSection() {
+    currentIndex++;
+    if (currentIndex == highRoadContent.length) currentIndex = 0;
+    highRoadTitleEl.textContent = highRoadContent[currentIndex].title;
+    highRoadTextEl.innerHTML = highRoadContent[currentIndex].text;
+    highRoadImgEls[0].src = highRoadContent[currentIndex].link;
+    highRoadImgEls[0].alt = highRoadContent[currentIndex].alt;
+    highRoadSignEl.className = highRoadContent[currentIndex].sign;
+
+    if (currentIndex + 1 < highRoadContent.length) {
+        highRoadImgEls[1].src = highRoadContent[currentIndex + 1].link;
+        highRoadImgEls[1].alt = highRoadContent[currentIndex + 1].alt;
+    } else {
+        highRoadImgEls[1].src = highRoadContent[0].link;
+        highRoadImgEls[1].alt = highRoadContent[0].alt;
+    }
 }
 
 function showPrevSection() {
-  currentIndex--;
-  if (currentIndex < 0) currentIndex = highRoadContent.length - 1;
-  highRoadTitleEl.textContent = highRoadContent[currentIndex].title;
-  highRoadTextEl.innerHTML = highRoadContent[currentIndex].text;
-  highRoadImgEls[0].src = highRoadContent[currentIndex].link;
-  highRoadImgEls[0].alt = highRoadContent[currentIndex].alt;
-  highRoadSignEl.className = highRoadContent[currentIndex].sign;
+    currentIndex--;
+    if (currentIndex < 0) currentIndex = highRoadContent.length - 1;
+    highRoadTitleEl.textContent = highRoadContent[currentIndex].title;
+    highRoadTextEl.innerHTML = highRoadContent[currentIndex].text;
+    highRoadImgEls[0].src = highRoadContent[currentIndex].link;
+    highRoadImgEls[0].alt = highRoadContent[currentIndex].alt;
+    highRoadSignEl.className = highRoadContent[currentIndex].sign;
 
-  if (currentIndex + 1 === highRoadContent.length) {
-    highRoadImgEls[1].src = highRoadContent[0].link;
-    highRoadImgEls[1].alt = highRoadContent[0].alt;
-  } else {
-    highRoadImgEls[1].src = highRoadContent[currentIndex + 1].link;
-    highRoadImgEls[1].alt = highRoadContent[currentIndex + 1].alt;
-  }
+    if (currentIndex + 1 === highRoadContent.length) {
+        highRoadImgEls[1].src = highRoadContent[0].link;
+        highRoadImgEls[1].alt = highRoadContent[0].alt;
+    } else {
+        highRoadImgEls[1].src = highRoadContent[currentIndex + 1].link;
+        highRoadImgEls[1].alt = highRoadContent[currentIndex + 1].alt;
+    }
 
 }
 
@@ -124,53 +133,52 @@ const bikesNav = page.querySelector('.bikes__nav');
 const bikesLinks = bikesNav.querySelectorAll('.bikes__radio');
 
 const bikesContent = [
-  {
-    name: 'Шоссе',
-    cards: [{
-      src: './images/Cervelo.jpg',
-      title: 'Cervelo Caledonia-5'
+    {
+        name: 'Шоссе',
+        cards: [{
+            src: './images/Cervelo.jpg',
+            title: 'Cervelo Caledonia-5'
+        },
+            {
+                src: './images/Cannondale.jpg',
+                title: 'Cannondale Systemsix Himod'
+            },
+            {
+                src: './images/treck.jpg',
+                title: 'Trek Domane SL-7'
+            }]
     },
     {
-      src: './images/Cannondale.jpg',
-      title: 'Cannondale Systemsix Himod'
+        name: 'Грэвел',
+        cards: [{
+            src: './images/cervelo-aspero.jpg',
+            title: 'Cervelo Aspero GRX 810'
+        },
+            {
+                src: './images/specialized-s.jpg',
+                title: 'Specialized S-Works Diverge'
+            },
+            {
+                src: './images/cannondale-topstone.jpg',
+                title: 'Cannondale Topstone Lefty 3'
+            }]
     },
     {
-      src: './images/treck.jpg',
-      title: 'Trek Domane SL-7'
-    }]
-  },
-  {
-    name: 'Грэвел',
-    cards: [{
-      src: './images/cervelo-aspero.jpg',
-      title: 'Cervelo Aspero GRX 810'
-    },
-    {
-      src: './images/specialized-s.jpg',
-      title: 'Specialized S-Works Diverge'
-    },
-    {
-      src: './images/cannondale-topstone.jpg',
-      title: 'Cannondale Topstone Lefty 3'
-    }]
-  },
-  {
-    name: 'TT',
-    cards: [{
-      src: './images/specialized-s-works-shiv.jpg',
-      title: 'Specialized S-Works Shiv'
-    },
-    {
-      src: './images/bmc.jpg',
-      title: 'BMC Timemachine 01 ONE'
-    },
-    {
-      src: './images/cervelo-p-series.jpg',
-      title: 'Cervelo P-Series'
-    }]
-  }
+        name: 'TT',
+        cards: [{
+            src: './images/specialized-s-works-shiv.jpg',
+            title: 'Specialized S-Works Shiv'
+        },
+            {
+                src: './images/bmc.jpg',
+                title: 'BMC Timemachine 01 ONE'
+            },
+            {
+                src: './images/cervelo-p-series.jpg',
+                title: 'Cervelo P-Series'
+            }]
+    }
 ]
-
 
 
 let category = bikesContent[0];
@@ -178,52 +186,53 @@ const bikesImgs = page.querySelectorAll('.bikes__img');
 const bikesNames = page.querySelectorAll('.bikes__name');
 
 for (let link of bikesLinks) {
-  link.addEventListener('click', bikesRadioBtnsHandler)
+    link.addEventListener('click', bikesRadioBtnsHandler)
 }
 
 function bikesRadioBtnsHandler(evt) {
-  const intViewportWidth = window.innerWidth;
-  if (intViewportWidth <= 740) {
-    openBikesMobileMenu(evt);
-  } else {
-    renderBikes(evt);
-    updateBikesMobileMenu();
-  }
+    const intViewportWidth = window.innerWidth;
+    if (intViewportWidth <= 740) {
+        openBikesMobileMenu(evt);
+    } else {
+        renderBikes(evt);
+        updateBikesMobileMenu();
+    }
 }
 
 const bikesLabels = bikesNav.querySelectorAll('.bikes__label');
+
 function openBikesMobileMenu(evt) {
-  bikesNav.classList.toggle('bikes__nav_opened');
-  bikesLabels.forEach(label => label.classList.remove('bikes__label_hidden'))
-  if (!bikesNav.classList.contains('bikes__nav_opened')) {
-    updateBikesMobileMenu();
-  }
-  renderBikes(evt)
+    bikesNav.classList.toggle('bikes__nav_opened');
+    bikesLabels.forEach(label => label.classList.remove('bikes__label_hidden'))
+    if (!bikesNav.classList.contains('bikes__nav_opened')) {
+        updateBikesMobileMenu();
+    }
+    renderBikes(evt)
 }
 
 function updateBikesMobileMenu() {
-  bikesLabels.forEach(label => label.classList.remove('bikes__label_hidden'));
-  for (let label of bikesLabels) {
-    if (!label.control.checked) {
-      label.classList.add('bikes__label_hidden');
+    bikesLabels.forEach(label => label.classList.remove('bikes__label_hidden'));
+    for (let label of bikesLabels) {
+        if (!label.control.checked) {
+            label.classList.add('bikes__label_hidden');
+        }
     }
-  }
 }
 
 function renderBikes(evt) {
-  const val = evt.target.value;
+    const val = evt.target.value;
 
-  for (let i = 0; i < bikesContent.length; i++) {
-    if (bikesContent[i].name === val) {
-      category = bikesContent[i];
-      break;
+    for (let i = 0; i < bikesContent.length; i++) {
+        if (bikesContent[i].name === val) {
+            category = bikesContent[i];
+            break;
+        }
     }
-  }
-  for (let i = 0; i < bikesImgs.length; i++) {
-    bikesImgs[i].src = category.cards[i].src;
-    bikesImgs[i].alt = category.cards[i].title;
-    bikesNames[i].textContent = category.cards[i].title;
-  }
+    for (let i = 0; i < bikesImgs.length; i++) {
+        bikesImgs[i].src = category.cards[i].src;
+        bikesImgs[i].alt = category.cards[i].title;
+        bikesNames[i].textContent = category.cards[i].title;
+    }
 
 }
 
@@ -235,17 +244,18 @@ const bikesCards = document.querySelector('.bikes__cards');
 const allCards = document.querySelectorAll('.bikes__card');
 let stepSize = allCards[0].clientWidth;
 let index = 0;
-
+// highRoadSliderWindow.style.maxHeight = "190px"
 sliderBtns.forEach(btn => btn.addEventListener('click', changePicture));
-window.onresize = function () {
-  bikesCards.style.transform = 'translateX(0px)';
-  sliderBtns[0].checked = true;
+
+function checkBikesCards() {
+    bikesCards.style.transform = 'translateX(0px)';
+    sliderBtns[0].checked = true;
 }
 
 function changePicture(evt) {
-  stepSize = allCards[0].clientWidth;
-  index = evt.target.value - 1;
-  bikesCards.style.transform = 'translateX(' + `${-stepSize * index}px)`;
+    stepSize = allCards[0].clientWidth;
+    index = evt.target.value - 1;
+    bikesCards.style.transform = 'translateX(' + `${-stepSize * index}px)`;
 }
 
 // swipe detection and handler
@@ -255,29 +265,29 @@ let touchstartX = 0;
 let touchendX = 0;
 
 sliderWindow.addEventListener('touchstart', function (event) {
-  touchstartX = event.changedTouches[0].screenX;
+    touchstartX = event.changedTouches[0].screenX;
 }, false);
 
 sliderWindow.addEventListener('touchend', function (event) {
-  touchendX = event.changedTouches[0].screenX;
-  handleGesture();
+    touchendX = event.changedTouches[0].screenX;
+    handleGesture();
 }, false);
 
 function handleGesture() {
-  const intViewportWidth = window.innerWidth;
-  if (intViewportWidth <= 740) {
-    stepSize = allCards[0].clientWidth;
-    if (touchendX < touchstartX) {
-      if (index === allCards.length - 1) index = -1;
-      index++;
-      bikesCards.style.transform = 'translateX(' + `${-stepSize * index}px)`;
-    } else {
-      if (index === 0) index = allCards.length;
-      index--;
-      bikesCards.style.transform = 'translateX(' + `${-stepSize * index}px)`;
+    const intViewportWidth = window.innerWidth;
+    if (intViewportWidth <= 740) {
+        stepSize = allCards[0].clientWidth;
+        if (touchendX < touchstartX) {
+            if (index === allCards.length - 1) index = -1;
+            index++;
+            bikesCards.style.transform = 'translateX(' + `${-stepSize * index}px)`;
+        } else {
+            if (index === 0) index = allCards.length;
+            index--;
+            bikesCards.style.transform = 'translateX(' + `${-stepSize * index}px)`;
+        }
+        sliderBtns[index].checked = true;
     }
-    sliderBtns[index].checked = true;
-  }
 }
 
 // footer form
@@ -288,6 +298,14 @@ const formInput = form.querySelector('.footer__input');
 form.addEventListener('submit', formSubmitHandler);
 
 function formSubmitHandler(evt) {
-  evt.preventDefault();
-  formInput.value = 'Круто!';
+    evt.preventDefault();
+    formInput.value = 'Круто!';
+}
+
+// window resizing changes
+
+window.onresize = function () {
+    checkBikesCards();
+    getHighRoadSliderWindow();
+
 }
